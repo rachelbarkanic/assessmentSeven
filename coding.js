4bcde4a5e8f8c290192598b436960ac13088acd1
Rachel Barkanic <rachel.barkanic@gmail.com>
2022-11-16 14:41:15 -0800

    need to complete prob 3

--- coding.js
+++ coding.js
@@ -18,18 +18,30 @@ for (let i = 0; i < array.length; i++) {
 
 console.log(sumfunc(array, target))
 
-//the runtime for this function is O(n^2)
+//the runtime of this function is O(n^2)
 
 //2. UNIQUE CHARACTERS
 
+const uniqueCharacters = (str) =>
+{
+     
+    for(let i = 0; i < str.length; i++)
+        for(let j = i + 1; j < str.length; j++)
+            if (str[i] == str[j])
+                return false;
+ 
+    return true;
+}
+ console.log(uniqueCharacters('lalala'))
 
+ //the runtime of this function is O(n^2)
 
 //3. PANGRAM SENTENCE
 
 //4. LONGEST WORD
 
 const findLongestWord = (str) => {
-    var longestWord = str.split(' ').reduce(function(longest, currentWord) {
+    let longestWord = str.split(' ').reduce(function(longest, currentWord) {
       return currentWord.length > longest.length ? currentWord : longest;
     }, "");
     return longestWord.length;
