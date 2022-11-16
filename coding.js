220365d1bb07249838b4ee1cc8dcd45e9dcdd860
Rachel Barkanic <rachel.barkanic@gmail.com>
2022-11-15 19:17:32 -0800

    prob 1 complete

--- coding.js
+++ coding.js
@@ -1,6 +1,28 @@
 //1. SUM ZERO
+let array = [1, 2, 3, 4, 0, -4]
+let target = 0
+
+const sumfunc = (array, target) => {
+let value = false
+for (let i = 0; i < array.length; i++) {
+  for (let j = 0; j < array.length; j++) 
+  if (i != j) {
+      if (array[i] + array[j] === target) {
+        value = true;
+      } 
+      }
+    }
+  return value
+  }
+
+
+console.log(sumfunc(array, target))
+
+//the runtime for this function is
 
 //2. UNIQUE CHARACTERS
+
+
 
 //3. PANGRAM SENTENCE
 
